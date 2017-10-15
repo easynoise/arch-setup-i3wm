cdd6199622d418699a21402439e11d6b9d17cc0e
easynoise <easynoise@live.de>
2017-10-15 15:39:28 +0200

    Create InstallYaourt.sh

---
+++ config/home/InstallYaourt.sh
@@ -0,0 +1,9 @@
+mkdir source && cd source
+git clone https://aur.archlinux.org/package-query.git
+cd package-query
+makepkg -si
+cd ..
+git clone https://aur.archlinux.org/yaourt.git
+cd yaourt
+makepkg -si
+cd ..
